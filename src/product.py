bd84f0934762e3b07c63f4662cf9517d04444d74
Artem Kulinich <kulini4perm@gmail.com>
2026-02-03 15:56:16 +0500

    feat: add exception handling for zero quantity and empty category price calculation

--- src/product.py
+++ src/product.py
@@ -15,6 +15,8 @@ class Product(BaseMixin, BaseProduct):
         self.name = name
         self.description = description
         self.__price = price
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         self.quantity = quantity
         super().__init__(name, description, price, quantity)
 
